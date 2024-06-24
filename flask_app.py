e06bc3964b256c3db6ec3bdd8f8e0f4405d72a51
Chelsea <chelseasingla123@gmail.com>
2024-06-24 23:11:29 +0530

    Added opal

--- flask_app.py
+++ flask_app.py
@@ -12,7 +12,7 @@ migrate = Migrate(app, db)
 
 permit = Permit(
     pdp="http://localhost:7766",
-    token="permit_key_Mfhhl1IiHOVtTx4BZL4taAvOCuiQRiGzB2Wyb79HipHs1RHhl79rN7xNH7v4x2K7CHNZA5BgngEuLNcfZLXgA2",
+    token="",
 )
 
 class User(db.Model):
